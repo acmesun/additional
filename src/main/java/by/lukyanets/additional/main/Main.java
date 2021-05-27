f9c08b541422a687b3a01d2e949b7790e32b97f6
acmesun <danfyou@gmail.com>
2021-05-27 14:37:40 +0300

    added properties files

--- src/main/java/by/lukyanets/additional/main/Main.java
+++ src/main/java/by/lukyanets/additional/main/Main.java
@@ -1,7 +1,35 @@
 package by.lukyanets.additional.main;
 
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("1 - eng\n2 - bel\nany - default");
+        char i = 0;
+        try {
+            i = (char) System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String country = "";
+        String language = "";
+        switch (i) {
+            case '1':
+                language = "en";
+                country = "US";
+                break;
+            case '2':
+                language = "be";
+                country = "BY";
+                break;
+        }
+        Locale current = new Locale(language, country);
+        ResourceBundle bundle = ResourceBundle.getBundle("text", current);
+        String s1 = bundle.getString("str1");
+        System.out.println(s1);
+        String s2 = bundle.getString("str2");
+        System.out.println(s2);
     }
 }
--- src/main/java/by/lukyanets/additional/manager/MessageManager.java
+++ src/main/java/by/lukyanets/additional/manager/MessageManager.java
@@ -1,4 +1,20 @@
 package by.lukyanets.additional.manager;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public enum MessageManager {
+    EN(ResourceBundle.getBundle("text", new Locale("en", "EN"))),
+    BY(ResourceBundle.getBundle("text", new Locale("be", "BY")));
+
+
+    private ResourceBundle bundle;
+
+    MessageManager(ResourceBundle bundle) {
+        this.bundle = bundle;
+    }
+
+    public String getString(String key) {
+        return bundle.getString(key);
+    }
 }
